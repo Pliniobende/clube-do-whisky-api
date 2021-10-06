6ce48cfc3698600765e601fe42dc0978c795a344
Plínio Bende Domingues da Silva <pliniobds@gmail.com>
2021-10-06 12:00:24 -0300

    services e controller Categories

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,37 +1,32 @@
+const categoriesServices = require('../services/categories.services');
+
 const categoriesController = {
 
     categoria: async (req, res) => {
-        let { id } = req.params;
-        let dados = await Categories.findOne({
-            where: {
-                id
+            try {
+                const { id } = req.params;
+    
+                const { data, status, error } = await categoriesServices.get(id);
+    
+                error ? res.status(status).json(error) : res.status(status).json(data);
+            } catch(e) {
+                res.status(500).json(e);
             }
-        })
-        if (dados){
-            let { name, description, detail} = dados;
-            res.send({
-            name,
-            description,
-            detail
-        });
-        }else{
-            res.status(404).send('Nenhum objeto encontrado');
         }
-        let dados2 = await Posts.findOne({
-            where: {
-                id
-            }
-        })
-        if (dados2){
-            let { image, description} = dados;
-            res.send({
-            image,
-            description
-        });
-        }else{
-            res.status(404).send('Nenhum objeto encontrado');
-        }
-    }
+        // let dados2 = await Posts.findAll({
+        //     where: {
+        //         id(FK)
+        //     }
+        // })
+        // if (dados2){
+        //     let { image, description} = dados;
+        //     res.send({
+        //     image,
+        //     description
+        // });
+        // }else{
+        //     res.status(404).send('Nenhum objeto encontrado');
+        // }
 };
 
 
