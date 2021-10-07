e5e40633ece80816fa1b241913158406d2f4cd9f
Plínio Bende Domingues da Silva <pliniobds@gmail.com>
2021-10-06 23:45:48 -0300

    service brands

---
+++ services/bands.services.js
@@ -0,0 +1,37 @@
+const { Categories } = require('../models');
+const { Brands } = require('../models');
+
+const brandsServices = {
+    get: async (id) => {
+        let status = null;
+        let error = null;
+        let data = {};
+        let brands = await Brands.findAll({
+            include: {
+                model: Categories[id],
+                as: 'Categories',
+                required: true
+            }
+        })
+        
+        try {
+            if (brands){
+            let { image, description } = brands;
+            data = {
+                image,
+                description
+            };
+        } else {
+            status = 404
+            res.send('Nenhuma marca encontrado')
+        }
+        } catch (e) {
+        status = 500;
+        error = e;
+        }
+
+        return { data, status, error }
+    }
+}
+
+module.exports = brandsServices;
