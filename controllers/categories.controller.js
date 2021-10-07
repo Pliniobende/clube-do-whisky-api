348b520444028e2778f8ed5c4ffbc4a46868a90b
Plínio Bende Domingues da Silva <pliniobds@gmail.com>
2021-10-06 23:44:30 -0300

    ajuste de rotas e controllers categories

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,19 +1,32 @@
 const categoriesServices = require('../services/categories.services');
+const brandsServices = require('../services/bands.services');
 
 const categoriesController = {
 
     categoria: async (req, res) => {
-            try {
-                const { id } = req.params;
+        try {
+            const { id } = req.params;
     
-                const { data, status, error } = await categoriesServices.get(id);
+            const { data, status, error } = await categoriesServices.get(id);
     
-                error ? res.status(status).json(error) : res.status(status).json(data);
-            } catch(e) {
-                res.status(500).json(e);
-            }
+            error ? res.status(status).json(error) : res.status(status).json(data);
+        } catch(e) {
+            res.status(500).json(e);
         }
-        
+    },
+
+    marca: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const { data, status, error } = await brandsServices.get(id);
+
+            error ? res.status(status).json(error) : res.status(status).json(data);
+        } catch(e) {
+            res.status(500).json(e);
+        }
+    }
+    
 };
 
 
--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -4,6 +4,7 @@ const categoriesController = require('../controllers/categories.controller');
 
 
 router.get('/:id', categoriesController.categoria);
+router.get('/:id', categoriesController.marca);
 
 
 module.exports = router;
