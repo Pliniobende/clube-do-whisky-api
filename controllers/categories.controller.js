0071ebe70a64be78d29ad8b3cacbeabd0f0fc214
Plínio Bende Domingues da Silva <pliniobds@gmail.com>
2021-10-08 19:14:02 -0300

    finalização Categories

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,33 +1,30 @@
-const categoriesServices = require('../services/categories.services');
-const brandsServices = require('../services/brands.services');
+const categoriesServices = require("../services/categories.services");
+const brandsServices = require("../services/brands.services");
 
 const categoriesController = {
+  categoria: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-    categoria: async (req, res) => {
-        try {
-            const { id } = req.params;
-    
-            const { data, status, error } = await categoriesServices.get(id);
-    
-            error ? res.status(status).json(error) : res.status(status).json(data);
-        } catch(e) {
-            res.status(500).json(e);
-        }
-    },
+      const { data, status, error } = await categoriesServices.get(id);
 
-    marca: async (req, res) => {
-        try {
-            const { id } = req.params;
-
-            const { data, status, error } = await brandsServices.get(id);
-
-            error ? res.status(status).json(error) : res.status(status).json(data);
-        } catch(e) {
-            res.status(500).json(e);
-        }
+      error ? res.status(status).json(error) : res.status(status).json(data);
+    } catch (e) {
+      res.status(500).json(e);
     }
-    
-};
+  },
 
+  marca: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const { data, status, error } = await brandsServices.getAll(id);
+
+      error ? res.status(status).json(error) : res.status(status).json(data);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  },
+};
 
 module.exports = categoriesController;
